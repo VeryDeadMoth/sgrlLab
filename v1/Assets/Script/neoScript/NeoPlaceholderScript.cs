bc14de0250dd4b98bbff4a8821136c428ea4c063
Xochil Toul <nix.axius@gmail.com>
2022-03-09 17:02:33 +0100

    Scene 3 (merge des scenes 1 et placeholder)
    merge des placeholders et de la balance

---
+++ v1/Assets/Script/neoScript/NeoPlaceholderScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeoPlaceholderScript : MonoBehaviour
+{
+    public GameObject balance;
+    float a = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void resetA()
+    {
+        a = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("hi");
+        if (balance.GetComponent<BalanceScript>().isOn)
+        {
+            //Debug.LogWarning(other);
+            if (other.gameObject.CompareTag("InteractCube"))
+            {
+                //Debug.Log("fuck off");
+                a = other.gameObject.GetComponent<MassCube>().g;
+                balance.GetComponent<BalanceScript>().updatePoids(a);
+                //txt.text = balance.GetComponent<BalanceScript>().currentPoids + "g";
+                //Debug.Log(txt.text);
+
+
+            }
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (balance.GetComponent<BalanceScript>().isOn)
+        {
+            balance.GetComponent<BalanceScript>().updatePoids(-a);
+            //txt.text = balance.GetComponent<BalanceScript>().currentPoids + "g";
+
+        }
+    }
+}
---
+++ v1/Assets/Script/neoScript/NeoRayCastMainCamera.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeoRayCastMainCamera : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    bool hasHit;
+    
+    public GameObject interactibleObject;
+    public NeoInteractObject interactObjectBehavior;
+    public bool mouseActive = true;
+
+    float durationMove = 0.5f;
+    float defaultZoom;
+    float dep;
+    float arr;
+    bool isZoomedIn = false;
+    public GameObject balance;
+    Vector3 init;
+
+void Start(){
+        defaultZoom = Camera.main.fieldOfView;
+        dep = defaultZoom;
+        arr = dep / 1.5f;
+    }
+
+void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        hasHit = Physics.Raycast(ray, out hit);
+        if (Input.GetMouseButtonDown(0) && mouseActive)
+        {
+            if (hasHit)
+            {
+                Debug.Log(hit.transform.name);
+
+                if (hit.transform.gameObject.CompareTag("InteractCube"))
+                {
+                    if (interactibleObject == null)
+                    {
+                        interactibleObject = hit.transform.gameObject;
+                        //Debug.Log("you can interact with me");
+                        {
+                            interactObjectBehavior.holdObject(interactibleObject);
+                            interactObjectBehavior.ReloadAllCubePlaceHolder();
+                            interactibleObject.layer = 2;
+                        }
+                    }
+                    else if (interactibleObject.CompareTag("pipette"))
+                    {
+                        //CHGMENT MASS CUBE
+                        //get placeholder in public val
+                        //modify a by new weight
+                        MassCube massCube = hit.transform.gameObject.GetComponent<MassCube>();
+                        massCube.g += 50;
+                        interactObjectBehavior.CallAnimPipette(hit.transform.gameObject);
+                    }
+                }
+                else if (hit.transform.gameObject.CompareTag("PlaceHolderBecher"))
+                {
+                    if (interactibleObject != null && interactibleObject.CompareTag("InteractCube"))
+                    {
+                        interactibleObject.layer = 0;
+                        //interactibleObject.transform.position = hit.transform.position;
+                        interactObjectBehavior.holdObject(hit.transform.gameObject);
+                        hit.transform.gameObject.SetActive(false);
+                        interactibleObject = null;
+                    }
+
+                }
+                else if (hit.transform.gameObject.CompareTag("PlaceHolderPipette"))
+                {
+                    if (interactibleObject != null && interactibleObject.CompareTag("pipette"))
+                    {
+                        interactibleObject.layer = 0;
+                        //interactibleObject.transform.position = hit.transform.position;
+                        interactObjectBehavior.holdObject(hit.transform.gameObject);
+                        hit.transform.gameObject.SetActive(false);
+                        interactibleObject = null;
+                    }
+
+                }
+                else if (hit.transform.CompareTag("pipette") && interactibleObject == null)
+                {
+                    interactibleObject = hit.transform.gameObject;
+                    interactObjectBehavior.holdObject(interactibleObject);
+                    interactObjectBehavior.ReloadAllCubePlaceHolder();
+                }
+                else if (hit.transform.CompareTag("bin"))
+                {
+                    if (interactibleObject.CompareTag("InteractCube"))
+                    {
+                        MassCube massCube = interactibleObject.GetComponent<MassCube>();
+                        massCube.g = 0;
+                    }
+                }
+                else if (hit.transform.CompareTag("balance")&&!isZoomedIn)
+                {
+                    isZoomedIn = true;
+                    //call coroutine
+                    StopAllCoroutines();
+                    init = new Vector3(balance.transform.position.x, 0, Camera.main.transform.position.z);
+                    StartCoroutine(MoveCamera(Camera.main.fieldOfView, arr,Camera.main.transform.position,init));
+                    
+                }
+            }
+            if (isZoomedIn&&!hasHit)
+            {
+                isZoomedIn = false;
+                StopAllCoroutines();
+                init = new Vector3(0.73f, 1, Camera.main.transform.position.z);
+                StartCoroutine(MoveCamera(Camera.main.fieldOfView, dep, Camera.main.transform.position, init));
+            }
+        }
+
+
+    }
+
+    IEnumerator MoveCamera(float targetDep, float targetArr, Vector3 initPos, Vector3 endPos)
+    {
+        float timeSince = 0;
+        while (timeSince < durationMove)
+        {
+            defaultZoom = Mathf.Lerp(targetDep, targetArr, timeSince / durationMove);
+            Camera.main.transform.position= Vector3.Lerp(initPos,endPos,timeSince/durationMove);
+            timeSince += Time.deltaTime;
+            Camera.main.fieldOfView = (defaultZoom);
+            //Debug.Log(defaultZoom);
+            yield return null;
+        }
+        Camera.main.transform.position = endPos;
+        defaultZoom = targetArr;
+    }
+}
