86442ccc028ab2d21bc4d35e0f30eaacd1a0a752
Xochil Toul <nix.axius@gmail.com>
2022-03-18 12:08:48 +0100

    Update cursor scene 2

--- v1/Assets/Script/Scene2Scripts/MouseMove.cs
+++ v1/Assets/Script/Scene2Scripts/MouseMove.cs
@@ -6,19 +6,22 @@ using UnityEngine.InputSystem;
 public class MouseMove : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Texture2D mouseCursor;
+    //public Texture2D mouseCursor;
+    Vector2 mousePos;
 
-    Vector2 hotSpot = new Vector2(0, 0);
-    CursorMode cursorMode = CursorMode.Auto;
+    //Vector2 hotSpot = new Vector2(0, 0);
+    //CursorMode cursorMode = CursorMode.Auto;
     void Start()
     {
-        //Cursor.visible = false;
-        Cursor.SetCursor(mouseCursor, hotSpot, cursorMode);
+        Cursor.visible = false;
+        //Cursor.SetCursor(mouseCursor, hotSpot, cursorMode);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mousePos);
+        transform.position = mousePos;
     }
 }
